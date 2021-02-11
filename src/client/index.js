db6bd36159ca593be637c239222a0cff123d39b8
Coderbora <coderbora@gmail.com>
2021-02-11 13:45:50 +0300

    initialize main client app w dependencies

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import App from './vue/App.vue';
+
+new Vue({ render: createElement => createElement(App) }).$mount('#app');
---
+++ src/client/vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        Hello, world!
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"    
+}
+</script>
+
+<style>
+</style>
