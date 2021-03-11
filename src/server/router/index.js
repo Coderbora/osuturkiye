56e06c2a88db2111123516b5635a42bcb32607f9
Coderbora <coderbora@gmail.com>
2021-03-11 19:40:19 +0300

    Revert "https rewrite"
    This reverts commit b190913b7547b5cb66ad4ae8f1df32ece217275a.

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -5,11 +5,6 @@ const ErrorCode = require("../models/ErrorCodes.js");
 
 const Logger = require("../Logger.js");
 
-// https re-write function
-http.get('*', function(req, res) {  
-    res.redirect('https://' + req.headers.host + req.url);
-})
-
 router.use("/api", apiRouter);
 
 router.use("/", express.static("build/client"));
