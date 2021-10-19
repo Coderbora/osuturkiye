4220973faf3ca69072ecaa0f9e3cd049c84b45e0
Coderbora <coderbora@gmail.com>
2021-10-19 20:21:57 +0300

    i forgor

--- src/server/router/api/index.ts
+++ src/server/router/api/index.ts
@@ -94,6 +94,7 @@ export class ApiRouter {
         
                 user.osu.playmode = profile._json.playmode;
                 user.osu.username = profile._json.username;
+                user.osu.country_code = profile._json.country_code;
         
                 user.osu.accessToken = accessToken;
                 user.osu.refreshToken = refreshToken;
